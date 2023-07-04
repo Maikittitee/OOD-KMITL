e49190d965fe6fda74d6ed1d87b6d6389e86a461
ktunchar <maikittitee1615@gmail.com>
2023-07-04 11:02:40 +0700

    fix : q ex2

--- wk01/best_prac#1/ex02.py
+++ wk01/best_prac#1/ex02.py
@@ -3,18 +3,20 @@ ip = input("Enter ID and Score end with ID : ").split()
 n_list = []
 s_list = []
 for i in range(len(ip)):
-    if (i % 2 == 0 and ip[i] in n_list):
-        ending = ip[i]
-        break
     if (i % 2 == 0):
         n_list.append(ip[i])
     else:
         s_list.append(float(ip[i]))
-
+target = ip[i]
 my_dict = dict(zip(n_list, s_list))
-sorted_dict = dict(sorted(my_dict.items(), key=lambda x:x[1]))
+# sorted_dict = dict(sorted(my_dict.items(), key=lambda x:x[1]))
 
+print(ip[0:-1])
+print(target)
 print(my_dict)
-print(ending)
-print(sorted_dict)
-print(ending[0])
+
+cnt = 0
+for i in my_dict.keys():
+    cnt += 1
+    if (i == target):
+        print(cnt)
