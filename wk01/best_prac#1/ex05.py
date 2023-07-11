868749b1c10f5b9beb76077be578392c0bc13d65
ktunchar <maikittitee1615@gmail.com>
2023-07-11 11:59:40 +0700

    sd

--- wk01/best_prac#1/ex05.py
+++ wk01/best_prac#1/ex05.py
@@ -1,66 +1,49 @@
-def	isPrime(n):
-		if n <= 1:
-			return False
-		if n <= 3:
-			return True
-		if n % 2 == 0 or n % 3 == 0:
-			return False
-
-		i = 5
-		while i * i <= n:
-			if n % i == 0 or n % (i + 2) == 0:
-				return False
-		i += 6
-		return True
-
 class MyInt:
-	def	__init__(self, n):
-		self.n = n
+    def __init__(self, number):
+        self.number = number
 
-	def isPrime(self):
-		if self.n < 2:
-			return False
-		for i in range(2, self.n):
-			if self.n% i == 0:
-				return False
-		return True
+    def __sub__(self, number):
+        return self.number - number.number//2
 
-	
-	def __str__(self):
-		return str(self.n)
+    def isPrime(self):
+        if self.number < 2:
+            return False
+        for i in range(2, self.number):
+            if self.number % i == 0:
+                return False
+        return True
 
-	def	showPrime(self):
+    def showPrime(self):
+        prime_numbers = []
+        for number in range(2, self.number + 1):
+            if self.isNumberPrime(number):
+                prime_numbers.append(str(number))
 
-		run = MyInt(2)
-		s = " ".join(str(n) for n in range(self.n + 1) if isPrime(n))
-		if s == "":
-			return ("!!!A prime number is a natural number greater than 1")
-		return (s)
+        if len(prime_numbers) == 0:
+            return "!!!A prime number is a natural number greater than 1"
+        return " ".join(prime_numbers)
 
-	def	__sub__(self, other):
-		return (self.n - other.n // 2)
-	
-	
-# a = MyInt(23)
+    def isNumberPrime(self, number):
+        if number < 2:
+            return False
+        for i in range(2, int(number**0.5) + 1):
+            if number % i == 0:
+                return False
+        return True
 
-# b = MyInt(17)
 
-# print(a.isPrime())
+print(" *** class MyInt ***")
+numbers = input("Enter 2 number : ").split()
+num1, num2 = int(numbers[0]), int(numbers[1])
 
-# print(b.isPrime())
+a = MyInt(num1)
+b = MyInt(num2)
 
-# print(a.showPrime())
+print(f"{num1} isPrime : {a.isPrime()}")
+print(f"{num2} isPrime : {b.isPrime()}")
 
-# print(b.showPrime())
+print(f"Prime number between 2 and {num1} : {a.showPrime()}")
+print(f"Prime number between 2 and {num2} : {b.showPrime()}")
 
-# print(a-b)
-
-print(" *** class MyInt *** ")
-n1, n2 = map(int, input("Enter 2 number : ").split())
-n1 = MyInt(n1)
-n2 = MyInt(n2)
-print(f"{n1} isPrime : {n1.isPrime()}")
-print(f"{n2} isPrime : {n2.isPrime()}")
-print(f"Prime number between 2 and {n1} : {n1.showPrime()}")
-print(f"Prime number between 2 and {n2} : {n2.showPrime()}")
-print(f"{n1} - {n2} = {n1 - n2}")
+result = num1 - round(num2 / 2)
+print(f'{a.number} - {b.number} = {a-b}')
