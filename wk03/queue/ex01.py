874f76c0d9105682a053c6a9f2674cf646b37a19
ktunchar <maikittitee1615@gmail.com>
2023-07-24 01:14:13 +0700

    queue ex01

--- wk03/queue/ex01.py
+++ wk03/queue/ex01.py
@@ -1,47 +1,40 @@
+class Queue:
+	def __init__(self, list=[]):
+		self.items = list
 
-class Stack :
-		def __init__(self,list = None) :
-			if list == None :
-				self.item = []
-			else :
-				self.item = list()
-		def	find(self, target: str):
-			for j in self.item:
-				if (self.item[j] == target):
-					return (j)
+	def enqueue(self, item):
+		self.items.append(item)
 
-		def isEmpty(self) :
-			pass
-                
-		def push(self,i) :
-			pass
+	def dequeue(self):
+		if self.items:
+			return self.items.pop(0)
 
-		def pop(self) :
-			pass
-                
-		def size(self) :
-			pass
+	def peek(self):
+		if self.items:
+			return self.items[0]
 
-def isMatchedHtml(raw):
-    S = Stack()
-    j = raw.find('<')
-    while j != -1:
-        k = raw.find('>', j+1)
-        if k == -1:
-            return False
-        tag = raw[j+1:k]
-        if not tag.startswith('/'):
-            S.push(tag) 
-        else:
-            if S.isEmpty(): 
-                return False
-            if tag[1:] != S.pop(): 
-                return False
-        j = raw.find('<' , k+1) 
-    return S.isEmpty()
+	def __len__(self):
+		return len(self.items)
 
-raw = input("Enter HTML content : ")
-if isMatchedHtml(raw):
-    print ("This is match tag HTML")
+	def __str__(self):
+		return str(self.items)
+
+	def __repr__(self):
+		return str(self)
+	
+q = Queue()
+s = input("Enter Input : ").split(",")
+for i in s:
+	if i[0] == 'E':
+		print(f"Add {i[2:]} index is {len(q)}")
+		q.enqueue(i[2:])
+	elif i[0] == 'D':
+		if len(q) != 0:
+			print(f"Pop {q.peek()} size in queue is {len(q)-1}")
+			q.dequeue()
+		else:
+			print("-1")
+if (len(q) == 0):
+	print("Empty")
 else:
-    print("This is not match tag HTML")
+	print(f"Number in Queue is :  {q}")
