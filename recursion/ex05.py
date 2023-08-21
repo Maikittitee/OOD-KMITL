722fcbaed54901e5b0b5cc273dd5e4c413fbc882
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2023-08-22 01:14:50 +0700

    ex05 last test case i wennn

--- recursion/ex05.py
+++ recursion/ex05.py
@@ -1,26 +1,65 @@
 
 
-def	is_colli(a:int, b:int):
-	if (a < 0 and b > 0):
-		return (True)
-	return (False)
+# def	is_colli(a:int, b:int):
+# 	if (a < 0 and b > 0):
+# 		return (True)
+# 	return (False)
 	  
 
-def asteroid_collision(curr: int, asts:list) -> list:
-	# base case
-	if ( len(asts) <= 1):
-		return ([asts[0]])
-	if (is_colli(curr, asts[0])):
-		if (abs(curr) > abs(asts[0])):
-			win = [curr]
-		elif (abs(curr) < abs(asts[0])):
-			win = [asts[0]]
-		else:
-			win = []
-	else:
-		win = [curr, asts[0]]
-	return (win + asteroid_collision(asts[0], asts[1:])) 
+# def remove_boom(defenders, attacker):
+# 	if (len(defenders) <= 0):
+# 		return
+# 	return ()
+	
 
-x = input("Enter Input : ").split(",")
+# def asteroid_collision(asts:list, l = []) -> list:
+# 	if (len(l) == 0):
+# 		l.append(asts[0])
+# 	else:
+# 		remove_boom(l, asts[0])
+	
+# 	pass
+def fn(i, st):
+	if not st:
+		st.append(i)
+		return
+	else:
+		if st[-1]<0:
+			st.append(i)
+			return
+		else:
+			if i>0:
+				st.append(i)
+				return
+			else:
+				if abs(i) == st[-1]:
+					st.pop()
+					return
+				else:
+					x = st.pop()
+					if abs(i)>abs(x):
+						fn(i, st)
+					else:
+						fn(x, st)
+					return
+				
+def ft_map_promax_ultra(st, aster):
+	if (len(aster) <= 0):
+		return
+	fn(aster[0], st)
+	ft_map_promax_ultra(st, aster[1:])
+	
+def asteroid_collision(aster: list) -> list:
+	st = []
+		# for i in aster:
+		#     fn(i, st)
+	ft_map_promax_ultra(st, aster)
+	return st
+
+x = input("Enter Input : ")
+if (' ' in x):
+	x = x.split(", ")
+else:
+	x = x.split(",")
 x = list(map(int,x))
-print(asteroid_collision(x[0], x[1:]))
+print(asteroid_collision(x))
