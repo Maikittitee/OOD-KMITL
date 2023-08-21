965aba9e27d14ccb3783f36f501318e7ded3c766
Krittithee Tuncharoen <maikittitee1615@gmail.com>
2023-08-21 22:59:25 +0700

    idk

---
+++ recursion/ex05.py
@@ -0,0 +1,16 @@
+
+
+def	is_colli(a:int, b:int):
+    if (a < 0 and b > 0):
+        return (True)
+    return (False)
+      
+
+def asteroid_collision(asts:list, curr:int) -> list:
+    if (colli):
+        return () 
+    
+
+x = input("Enter Input : ").split(",")
+x = list(map(int,x))
+print(asteroid_collision(x[1:], x[0]))
