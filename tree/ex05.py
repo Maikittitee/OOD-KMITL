e8a29629cee08cce88e1941c4b507bf90e75a52d
ktunchar <maikittitee1615@gmail.com>
2023-09-12 09:49:38 +0700

    gu mai ru jaa

--- tree/ex05.py
+++ tree/ex05.py
@@ -35,21 +35,53 @@ class Node:
 		return (1 + Node.count_l_space(node.left))
 
 
-	def print_promax(node: "Node", root, nl = 1, deep = 0):
-		if (node == None):
-			return 
-		print(' ' * Node.count_l_space(node.left), end = '')
-		if (nl == 1):
-			print(node.data)
-		else:
-			print(node.data, end = '')
-		curr = root
-		i = 0
-		while (curr != None):
-			if (i == deep):
-				print(curr.deep,end = '')
-			i += 1
-		
+	def print_promax(node):
+		"""Returns list of strings, width, height, and horizontal coordinate of the root."""
+		# No child.
+		if node.right is None and node.left is None:
+			line = '%s' % node.data
+			width = len(line)
+			height = 1
+			middle = width // 2
+			return [line], width, height, middle
+
+		# Only left child.
+		if node.right is None:
+			lines, n, p, x = node.left.print_promax()
+			s = '%s' % node.data
+			u = len(s)
+			first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+			second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+			shifted_lines = [line + u * ' ' for line in lines]
+			return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+		# Only right child.
+		if node.left is None:
+			lines, n, p, x = node.right.print_promax()
+			s = '%s' % node.data
+			u = len(s)
+			first_line = s + x * '_' + (n - x) * ' '
+			second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+			shifted_lines = [u * ' ' + line for line in lines]
+			return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+		# Two children.
+		left, n, p, x = node.left.print_promax()
+		right, m, q, y = node.right.print_promax()
+		s = '%s' % node.data
+		u = len(s)
+		first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+		second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+		if p < q:
+			left += [n * ' '] * (q - p)
+		elif q < p:
+			right += [m * ' '] * (p - q)
+		zipped_lines = zip(left, right)
+		lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+		return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+	
+	
 
 	def print(node, deep = 0):
 		if (node == None):
@@ -66,8 +98,13 @@ class BST:
 		self.root = Node.insert(self.root, new_data)
 
 	def print_promax(self):
-		Node.print_promax(self.root)
-	
+		return (Node.print_promax(self.root))
+
+	def print_promax_ultra(self):
+		lines, a, b, c = self.print_promax()
+		for line in lines:
+			print(line)
+
 	def print(self):
 		Node.print(self.root)
 
@@ -75,8 +112,8 @@ t = BST()
 inp = [int(i) for i in input('Enter Input : ').split()]
 for i in inp:
 	root = t.insert(i)
-t.print_promax()
+t.print_promax_ultra()
 
-print("-----------------")
-t.print()
+# print("-----------------")
+# t.print()
 	
