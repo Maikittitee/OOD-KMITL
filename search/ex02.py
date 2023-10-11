66e08e7300de3f5e082a137396aa0d63223590c8
ktunchar <maikittitee1615@gmail.com>
2023-10-12 00:58:33 +0700

    idk

--- search/ex02.py
+++ search/ex02.py
@@ -1,3 +1,15 @@
 inp = input("Enter Input : ").split('/')
 nums = list(map(int, inp[0].split()))
 targets = list(map(int, inp[1].split()))
+
+for target in targets:
+	min = None
+	for i in range(1, 1000):
+		if (target + i in nums):
+			min = target + i
+			break
+	if (min):
+		print(min)
+	else:
+		print("No First Greater Value")
+
